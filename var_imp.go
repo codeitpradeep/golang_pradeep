175e0ef88c787334071e777bd907ddc787c9b839
Pradeep <pradeepdhanush97@gmail.com>
2024-08-06 23:58:37 +0530

    new

---
+++ var_imp.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var nameone string = "nameone"
+	var nametwo = "name2"
+	var namethree string
+	fmt.Println("Var", nameone, nametwo, "----", namethree)
+
+	namethree = "prad"
+	namefour := "new"
+
+	fmt.Println(namethree, namefour)
+
+	var age int = 28
+
+	fmt.Println(age)
+
+}
